4413d4fa4232895b0ab3978d2b86ebaba0d6f927
MegaKruk <filip.mazur.dev@gmail.com>
2019-02-15 21:47:01 +0100

    lab4

---
+++ Zajecia_nr_4/Zadanie/_build/nrf52832_xxaa/icmp6.c.d
@@ -0,0 +1,191 @@
+_build/nrf52832_xxaa/icmp6.c.o: \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/iot/ipv6_stack/icmp6/icmp6.c \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/stdio.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/_ansi.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/newlib.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/config.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/ieeefp.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/features.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/cdefs.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/_default_types.h \
+ /home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stddef.h \
+ /home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdarg.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/reent.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/_ansi.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_types.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/_types.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/lock.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/types.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_stdint.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/types.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/stdio.h \
+ /home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/stdint.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_intsup.h \
+ /home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/string.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/string.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/iot/ipv6_stack/include/icmp6_api.h \
+ config/sdk_config.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/sdk_common.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/nordic_common.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/device/compiler_abstraction.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/sdk_os.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/sdk_errors.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/softdevice/s132/headers/nrf_error.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/app_util.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/device/nrf.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/device/nrf52.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/toolchain/cmsis/include/core_cm4.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/toolchain/cmsis/include/core_cmInstr.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/toolchain/cmsis/include/cmsis_gcc.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/toolchain/cmsis/include/core_cmFunc.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/toolchain/cmsis/include/core_cmSimd.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/toolchain/system_nrf52.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/device/nrf52_bitfields.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/device/nrf51_to_nrf52.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/device/nrf52_name_change.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/device/compiler_abstraction.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/sdk_macros.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/iot/common/iot_defines.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/iot/ipv6_stack/include/ipv6_api.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/iot/common/iot_common.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/softdevice/s132/headers/ble_gap.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/softdevice/s132/headers/ble_types.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/softdevice/s132/headers/ble_ranges.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/softdevice/s132/headers/nrf_svc.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/iot/common/iot_defines.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/iot/common/iot_errors.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/sdk_errors.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/sdk_os.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/iot/ipv6_stack/pbuffer/iot_pbuffer.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/iot/ipv6_stack/include/ipv6_api.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/iot/ipv6_stack/icmp6/icmp6.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/iot/context_manager/iot_context_manager.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/iot/ipv6_stack/utils/ipv6_utils.h
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/stdio.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/_ansi.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/newlib.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/config.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/ieeefp.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/features.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/cdefs.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/_default_types.h:
+
+/home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stddef.h:
+
+/home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdarg.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/reent.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/_ansi.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_types.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/_types.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/lock.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/types.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_stdint.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/types.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/stdio.h:
+
+/home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/stdint.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_intsup.h:
+
+/home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/string.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/string.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/iot/ipv6_stack/include/icmp6_api.h:
+
+config/sdk_config.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/sdk_common.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/nordic_common.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/device/compiler_abstraction.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/sdk_os.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/sdk_errors.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/softdevice/s132/headers/nrf_error.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/app_util.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/device/nrf.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/device/nrf52.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/toolchain/cmsis/include/core_cm4.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/toolchain/cmsis/include/core_cmInstr.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/toolchain/cmsis/include/cmsis_gcc.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/toolchain/cmsis/include/core_cmFunc.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/toolchain/cmsis/include/core_cmSimd.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/toolchain/system_nrf52.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/device/nrf52_bitfields.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/device/nrf51_to_nrf52.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/device/nrf52_name_change.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/device/compiler_abstraction.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/sdk_macros.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/iot/common/iot_defines.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/iot/ipv6_stack/include/ipv6_api.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/iot/common/iot_common.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/softdevice/s132/headers/ble_gap.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/softdevice/s132/headers/ble_types.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/softdevice/s132/headers/ble_ranges.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/softdevice/s132/headers/nrf_svc.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/iot/common/iot_defines.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/iot/common/iot_errors.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/sdk_errors.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/sdk_os.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/iot/ipv6_stack/pbuffer/iot_pbuffer.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/iot/ipv6_stack/include/ipv6_api.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/iot/ipv6_stack/icmp6/icmp6.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/iot/context_manager/iot_context_manager.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/iot/ipv6_stack/utils/ipv6_utils.h:
---
+++ Zajecia_nr_4/Zadanie/_build/nrf52832_xxaa/nrf_fstorage.c.d
@@ -0,0 +1,44 @@
+_build/nrf52832_xxaa/nrf_fstorage.c.o: \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/libraries/fstorage/nrf_fstorage.c \
+ config/sdk_config.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/libraries/fstorage/nrf_fstorage.h \
+ /home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/stdint.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/_default_types.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/features.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_intsup.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_stdint.h \
+ /home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/sdk_errors.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/softdevice/s132/headers/nrf_error.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/libraries/experimental_section_vars/nrf_section.h \
+ /home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/nordic_common.h \
+ /home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stddef.h
+
+config/sdk_config.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/libraries/fstorage/nrf_fstorage.h:
+
+/home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/stdint.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/_default_types.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/features.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_intsup.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_stdint.h:
+
+/home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/sdk_errors.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/softdevice/s132/headers/nrf_error.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/libraries/experimental_section_vars/nrf_section.h:
+
+/home/susers/s226018/swiot/iot-lab/sdk/components/libraries/util/nordic_common.h:
+
+/home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stddef.h:
