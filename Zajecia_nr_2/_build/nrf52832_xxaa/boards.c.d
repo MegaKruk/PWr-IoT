4350d2968a07f4d1c15a0503274059f525160dee
MegaKruk <filip.mazur.dev@gmail.com>
2019-02-15 21:33:49 +0100

    lab2

---
+++ Zajecia_nr_2/_build/nrf52832_xxaa/boards.c.d
@@ -0,0 +1,146 @@
+_build/nrf52832_xxaa/boards.c.o: ../../sdk/components/boards/boards.c \
+ ../../sdk/components/boards/boards.h \
+ ../../sdk/components/drivers_nrf/hal/nrf_gpio.h \
+ ../../sdk/components/device/nrf.h ../../sdk/components/device/nrf52.h \
+ ../../sdk/components/toolchain/cmsis/include/core_cm4.h \
+ /home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/stdint.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/_default_types.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/features.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_intsup.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_stdint.h \
+ ../../sdk/components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../sdk/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../sdk/components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../sdk/components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../sdk/components/toolchain/system_nrf52.h \
+ ../../sdk/components/device/nrf52_bitfields.h \
+ ../../sdk/components/device/nrf51_to_nrf52.h \
+ ../../sdk/components/device/nrf52_name_change.h \
+ ../../sdk/components/device/compiler_abstraction.h \
+ ../../sdk/components/drivers_nrf/hal/nrf_peripherals.h \
+ ../../sdk/components/device/nrf52832_peripherals.h \
+ ../../sdk/components/libraries/util/nrf_assert.h \
+ ../../sdk/components/libraries/util/app_error.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/stdio.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/_ansi.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/newlib.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/config.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/ieeefp.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/cdefs.h \
+ /home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stddef.h \
+ /home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdarg.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/reent.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/_ansi.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_types.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/_types.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/lock.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/types.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/types.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/stdio.h \
+ /home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h \
+ ../../sdk/components/libraries/util/sdk_errors.h \
+ ../../sdk/components/drivers_nrf/nrf_soc_nosd/nrf_error.h sdk_config.h \
+ ../../sdk/components/libraries/util/nordic_common.h \
+ ../../sdk/components/libraries/util/app_error_weak.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/stdlib.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/stdlib.h \
+ ../../sdk/components/boards/pca10040.h
+
+../../sdk/components/boards/boards.h:
+
+../../sdk/components/drivers_nrf/hal/nrf_gpio.h:
+
+../../sdk/components/device/nrf.h:
+
+../../sdk/components/device/nrf52.h:
+
+../../sdk/components/toolchain/cmsis/include/core_cm4.h:
+
+/home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/stdint.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/_default_types.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/features.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_intsup.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_stdint.h:
+
+../../sdk/components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../sdk/components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../sdk/components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../sdk/components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../sdk/components/toolchain/system_nrf52.h:
+
+../../sdk/components/device/nrf52_bitfields.h:
+
+../../sdk/components/device/nrf51_to_nrf52.h:
+
+../../sdk/components/device/nrf52_name_change.h:
+
+../../sdk/components/device/compiler_abstraction.h:
+
+../../sdk/components/drivers_nrf/hal/nrf_peripherals.h:
+
+../../sdk/components/device/nrf52832_peripherals.h:
+
+../../sdk/components/libraries/util/nrf_assert.h:
+
+../../sdk/components/libraries/util/app_error.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/stdio.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/_ansi.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/newlib.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/config.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/ieeefp.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/cdefs.h:
+
+/home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stddef.h:
+
+/home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdarg.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/reent.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/_ansi.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_types.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/_types.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/lock.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/types.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/types.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/stdio.h:
+
+/home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h:
+
+../../sdk/components/libraries/util/sdk_errors.h:
+
+../../sdk/components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
+
+sdk_config.h:
+
+../../sdk/components/libraries/util/nordic_common.h:
+
+../../sdk/components/libraries/util/app_error_weak.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/stdlib.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/stdlib.h:
+
+../../sdk/components/boards/pca10040.h:
---
+++ Zajecia_nr_2/_build/nrf52832_xxaa/nrf_log_frontend.c.d
@@ -0,0 +1,123 @@
+_build/nrf52832_xxaa/nrf_log_frontend.c.o: \
+ ../../sdk/components/libraries/experimental_log/src/nrf_log_frontend.c \
+ ../../sdk/components/libraries/util/sdk_common.h \
+ /home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/stdint.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/_default_types.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/features.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_intsup.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_stdint.h \
+ /home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/string.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/_ansi.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/newlib.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/config.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/ieeefp.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/reent.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/_ansi.h \
+ /home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stddef.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_types.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/_types.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/lock.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/cdefs.h \
+ /home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/string.h \
+ sdk_config.h ../../sdk/components/libraries/util/nordic_common.h \
+ ../../sdk/components/device/compiler_abstraction.h \
+ ../../sdk/components/libraries/util/sdk_os.h \
+ ../../sdk/components/libraries/util/sdk_errors.h \
+ ../../sdk/components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ ../../sdk/components/libraries/util/app_util.h \
+ ../../sdk/components/device/nrf.h ../../sdk/components/device/nrf52.h \
+ ../../sdk/components/toolchain/cmsis/include/core_cm4.h \
+ ../../sdk/components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../sdk/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../sdk/components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../sdk/components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../sdk/components/toolchain/system_nrf52.h \
+ ../../sdk/components/device/nrf52_bitfields.h \
+ ../../sdk/components/device/nrf51_to_nrf52.h \
+ ../../sdk/components/device/nrf52_name_change.h \
+ ../../sdk/components/device/compiler_abstraction.h \
+ ../../sdk/components/libraries/util/sdk_macros.h
+
+../../sdk/components/libraries/util/sdk_common.h:
+
+/home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/stdint.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/_default_types.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/features.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_intsup.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_stdint.h:
+
+/home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/string.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/_ansi.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/newlib.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/config.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/ieeefp.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/reent.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/_ansi.h:
+
+/home/susers/s226018/swiot/gcc/lib/gcc/arm-none-eabi/4.9.3/include/stddef.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/_types.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/machine/_types.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/lock.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/cdefs.h:
+
+/home/susers/s226018/swiot/gcc/arm-none-eabi/include/sys/string.h:
+
+sdk_config.h:
+
+../../sdk/components/libraries/util/nordic_common.h:
+
+../../sdk/components/device/compiler_abstraction.h:
+
+../../sdk/components/libraries/util/sdk_os.h:
+
+../../sdk/components/libraries/util/sdk_errors.h:
+
+../../sdk/components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
+
+../../sdk/components/libraries/util/app_util.h:
+
+../../sdk/components/device/nrf.h:
+
+../../sdk/components/device/nrf52.h:
+
+../../sdk/components/toolchain/cmsis/include/core_cm4.h:
+
+../../sdk/components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../sdk/components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../sdk/components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../sdk/components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../sdk/components/toolchain/system_nrf52.h:
+
+../../sdk/components/device/nrf52_bitfields.h:
+
+../../sdk/components/device/nrf51_to_nrf52.h:
+
+../../sdk/components/device/nrf52_name_change.h:
+
+../../sdk/components/device/compiler_abstraction.h:
+
+../../sdk/components/libraries/util/sdk_macros.h:
